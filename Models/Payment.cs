5d32f74570357658bbd14a36d2a677ff91f9bab6
latha.thavamani@gmail.com <Tdouble#222>
2023-07-16 09:48:14 +0000

    Validation issue is fixed

--- Models/Payment.cs
+++ Models/Payment.cs
@@ -5,13 +5,13 @@ namespace PaymentGateway.Models
     public class Payment
     {
         public string? Id { get; set; }
-        [StringLength(12,ErrorMessage ="Card number should be 12 characters")]
+        [StringLength(12,MinimumLength =12,ErrorMessage ="Card number should be 12 characters")]
         public string CardNumber { get; set; }
         public string ExpirtMonthDate { get; set; }
         public double amount { get; set; }
-        [StringLength(3, ErrorMessage = "Currency should be 3 characters")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "Currency should be 3 characters")]
         public string Currency { get; set; }
-        [Range(100,999,ErrorMessage = "Card number should be 3 digits number")]
+        [Range(100,999,ErrorMessage = "CVV should be 3 digits range between 000-999")]
         public int CVV { get; set; }
     }
 }
