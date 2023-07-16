83293ea015e9c668f4609992556fe83d3a8946d2
latha.thavamani@gmail.com <Tdouble#222>
2023-07-16 03:55:59 +0000

    Added validation for Payment model

--- Models/Payment.cs
+++ Models/Payment.cs
@@ -1,12 +1,17 @@
-﻿namespace PaymentGateway.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentGateway.Models
 {
     public class Payment
     {
         public string? Id { get; set; }
+        [StringLength(12,ErrorMessage ="Card number should be 12 characters")]
         public string CardNumber { get; set; }
         public string ExpirtMonthDate { get; set; }
         public double amount { get; set; }
+        [StringLength(3, ErrorMessage = "Currency should be 3 characters")]
         public string Currency { get; set; }
+        [Range(100,999,ErrorMessage = "Card number should be 3 digits number")]
         public int CVV { get; set; }
     }
 }
